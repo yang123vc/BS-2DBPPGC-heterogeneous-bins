55ec55fd6cce9dcc76bb0fa999b4ce4c7cf29d58
Carlos Alegría <calegria+gh@gmail.com>
2017-06-29 14:58:03 -0500

    Refactoring code

---
+++ README.md
@@ -0,0 +1,2 @@
+# BS-4-2DBPPGC-heterogeneous-bins
+A Beam Search implementation for the two-dimmensional Bin Packing Problem with Guillotine Cuts (2D-BPPGC) using heterogeneous (different sized) bins
--- src/ConstructGlobalSolution.cpp
+++ src/ConstructGlobalSolution.cpp
@@ -20,16 +20,16 @@
 #include "classes_BPGC_HetBins.h"
 
 void
-OpenBin (NODE &bin, PIEZA &item);
+OpenBin(NODE &bin, PIEZA &item);
 
 void
-fill_node (NODE &current_bin, vector<PIEZA> &pzas);
+fill_node(NODE &current_bin, vector<PIEZA> &pzas);
 
 vector<PIEZA>
-set_available_pzas (NODE &father, vector<PIEZA> &all_pzas);
+set_available_pzas(NODE &father, vector<PIEZA> &all_pzas);
 
 double
-AreaToPack (NODE node, vector<PIEZA> pzas);
+AreaToPack(NODE node, vector<PIEZA> pzas);
 
 ///
 /// Estimates a solution with the given pieces.
@@ -45,101 +45,94 @@ AreaToPack (NODE node, vector<PIEZA> pzas);
 ///				estimated solution.
 /// @param[out]	bin_sol		The estimated solution.
 ///
-void
-Construct_Solution (const char *bin_name, vector<PIEZA> pzas, double level,
-		    double &A_bin, double &Amax, double &of_value,
-		    vector<NODE> &bin_sol)
-{
-  // clean output variables
-  //
-  bin_sol.clear ();
-  of_value = 0;
+void Construct_Solution(const char *bin_name, vector<PIEZA> pzas, double level,
+		double &A_bin, double &Amax, double &of_value, vector<NODE> &bin_sol) {
+	// clean output variables
+	//
+	bin_sol.clear();
+	of_value = 0;
 
-  // vector of size btype_size for each type of bin, at each level.
-  //
-  vector<NODE> current_bin;
+	// vector of size btype_size for each type of bin, at each level.
+	//
+	vector<NODE> current_bin;
 
-  // reads type of bin in file.
-  //
-  int bsizes = 0;
+	// reads type of bin in file.
+	//
+	int bsizes = 0;
 
-  string taux;
-  string cadena;
-  string line;
-  vector<string> btype;
-  ifstream b_file (bin_name);
-  if (!b_file.is_open ())
-    {
-      cout << "ERROR: Unable to open file " << bin_name << "\n";
-      exit (1);
-    }
-  getline (b_file, line); //'Size'  'L'  'W'
-  //Create a btype vector, that holds each type of bin 0 = 'S', 1= 'M'...
-  while (!b_file.eof ())
-    {
-      NODE naux;
-      b_file >> cadena;    //Reads type of bin.
-      taux = cadena;
-      btype.push_back (taux);
-      getline (b_file, line);
-      ++bsizes;
-      naux.initialize_node (bin_name, bsizes, btype[bsizes - 1]);
-      current_bin.push_back (naux);
-    }
-  //==========================
-  //Placing the first piece in the first bin that fits.
-  vector<int> ID_placed;
-  while (ID_placed.size () < pzas.size ())    //Until all pieces are placed.
-    {
-      int b = 0;
-      int p = 0;
-      //Access to the first piece not in the ID_placed
-      while (find (ID_placed.begin (), ID_placed.end (), pzas[p].getID ())
-	  != ID_placed.end ())
-	p++;
-      while (pzas[p].getArea () > current_bin[b].get_waste ()
-	  && b < current_bin.size ())
-	b++;
-      double best_bin_use = -GRANDE;
-      double worst_bin_use = GRANDE;
-
-      int best_binIdx = 0;
-      for (int bb = b; bb < current_bin.size (); bb++)
+	string taux;
+	string cadena;
+	string line;
+	vector<string> btype;
+	ifstream b_file(bin_name);
+	if (!b_file.is_open()) {
+		cout << "ERROR: Unable to open file " << bin_name << "\n";
+		exit(1);
+	}
+	getline(b_file, line); //'Size'  'L'  'W'
+	//Create a btype vector, that holds each type of bin 0 = 'S', 1= 'M'...
+	while (!b_file.eof()) {
+		NODE naux;
+		b_file >> cadena;    //Reads type of bin.
+		taux = cadena;
+		btype.push_back(taux);
+		getline(b_file, line);
+		++bsizes;
+		naux.initialize_node(bin_name, bsizes, btype[bsizes - 1]);
+		current_bin.push_back(naux);
+	}
+	//==========================
+	//Placing the first piece in the first bin that fits.
+	vector<int> ID_placed;
+	while (ID_placed.size() < pzas.size())    //Until all pieces are placed.
 	{
-	  //open a bin
-	  if (bin_sol.empty ())    //Update pzas disponibles
-	    current_bin[bb].set_ID_pzas_disp (pzas);
-	  else //some pieces have been placed,
-	       //Need to retrieve them and update pzas disponibles.
-	    {
-	      vector<PIEZA> p_disp;
-	      p_disp = set_available_pzas (bin_sol[bin_sol.size () - 1], pzas);
-	      current_bin[bb].set_ID_pzas_disp (p_disp);
-	    }
-	  OpenBin (current_bin[bb], pzas[p]);
-	  if (!current_bin[bb].getPI ().empty ())
-	    {
-	      //Fill the bin
-	      fill_node (current_bin[bb], pzas);
-	      //============================================================
-	      //(A) Keep the best of all current_bin in terms of %utilization.
-	      //=========================================================
+		int b = 0;
+		int p = 0;
+		//Access to the first piece not in the ID_placed
+		while (find(ID_placed.begin(), ID_placed.end(), pzas[p].getID())
+				!= ID_placed.end())
+			p++;
+		while (pzas[p].getArea() > current_bin[b].get_waste()
+				&& b < current_bin.size())
+			b++;
+		double best_bin_use = -GRANDE;
+		double worst_bin_use = GRANDE;
+
+		int best_binIdx = 0;
+		for (int bb = b; bb < current_bin.size(); bb++) {
+			//open a bin
+			if (bin_sol.empty())    //Update pzas disponibles
+				current_bin[bb].set_ID_pzas_disp(pzas);
+			else //some pieces have been placed,
+				 //Need to retrieve them and update pzas disponibles.
+			{
+				vector<PIEZA> p_disp;
+				p_disp = set_available_pzas(bin_sol[bin_sol.size() - 1], pzas);
+				current_bin[bb].set_ID_pzas_disp(p_disp);
+			}
+			OpenBin(current_bin[bb], pzas[p]);
+			if (!current_bin[bb].getPI().empty()) {
+				//Fill the bin
+				fill_node(current_bin[bb], pzas);
+				//============================================================
+				//(A) Keep the best of all current_bin in terms of %utilization.
+				//=========================================================
 //                if (current_bin[bb].getPropUtil() > best_bin_use)
 //                {
 //                    best_bin_use = current_bin[bb].getPropUtil();
 //                    best_binIdx = bb;
 //                }
-	      //=========================================================
-	      //(B) Keep the best of all current_bin in terms of Absolute Waste.
-	      //==============================================================
+				//=========================================================
+				//(B) Keep the best of all current_bin in terms of Absolute Waste.
+				//==============================================================
 //                if (current_bin[bb].get_waste() < worst_bin_use)
 //                {
 //                    worst_bin_use = current_bin[bb].get_waste();
 //                    best_binIdx = bb;
 //                }
-	      //============================================================
-	      //(C) Keep the best of all current bin, in terms of (AbsWaste/#pieces in bin)*#pieces to pack.
-	      //============================================================
+				//============================================================
+				//(C) Keep the best of all current bin, in terms of (AbsWaste/#pieces in bin)*#pieces to pack.
+				//============================================================
 //               double of_select = 0 ;
 //                of_select = current_bin[bb].get_waste()/current_bin[bb].getNumPiezas();
 //                of_select = of_select * current_bin[bb].get_IDdisp().size();
@@ -148,9 +141,9 @@ Construct_Solution (const char *bin_name, vector<PIEZA> pzas, double level,
 //                    worst_bin_use = of_select;
 //                    best_binIdx = bb;
 //                }
-	      //============================================================
-	      //(D) Keep the best of all current bin, in terms of  (%Utilization/#pieces in bin)*#pieces to pack
-	      //============================================================
+				//============================================================
+				//(D) Keep the best of all current bin, in terms of  (%Utilization/#pieces in bin)*#pieces to pack
+				//============================================================
 //                double of_select = 0 ;
 //                of_select = current_bin[bb].getPropUtil()/current_bin[bb].getNumPiezas();
 //                of_select = of_select * current_bin[bb].get_IDdisp().size();
@@ -159,9 +152,9 @@ Construct_Solution (const char *bin_name, vector<PIEZA> pzas, double level,
 //                    best_bin_use = of_select;
 //                    best_binIdx = bb;
 //                }
-	      //============================================================
-	      //(E) Keep the min (Abs waste/ total Area of pieces in bin) × (area pieces to pack /#pieces left to pack)
-	      //============================================================
+				//============================================================
+				//(E) Keep the min (Abs waste/ total Area of pieces in bin) × (area pieces to pack /#pieces left to pack)
+				//============================================================
 //                double of_select = 0 ;
 //                of_select = current_bin[bb].get_waste();
 //                double area_bin=0;
@@ -184,20 +177,19 @@ Construct_Solution (const char *bin_name, vector<PIEZA> pzas, double level,
 //                    worst_bin_use = of_select;
 //                    best_binIdx = bb;
 //                }
-	      //============================================================
-	      //(E) Keep the min suma[(Area pza)^2/Amax(por empaquetar)
-	      //Discussed with Toni
-	      //============================================================
-	      double of_select = 0;
-	      double area_pzas_bin = 0;
-	      for (int i = 0; i < current_bin[bb].getNumPiezas (); i++)
-		{
-		  PIEZA p = *current_bin[bb].getPI ()[i];
-		  area_pzas_bin = area_pzas_bin + pow (p.getArea (), 2);
-		}
-	      double area_bin = current_bin[bb].getW ()
-		  * current_bin[bb].getL ();
-	      of_select = area_pzas_bin / (area_bin * Amax);
+				//============================================================
+				//(E) Keep the min suma[(Area pza)^2/Amax(por empaquetar)
+				//Discussed with Toni
+				//============================================================
+				double of_select = 0;
+				double area_pzas_bin = 0;
+				for (int i = 0; i < current_bin[bb].getNumPiezas(); i++) {
+					PIEZA p = *current_bin[bb].getPI()[i];
+					area_pzas_bin = area_pzas_bin + pow(p.getArea(), 2);
+				}
+				double area_bin = current_bin[bb].getW()
+						* current_bin[bb].getL();
+				of_select = area_pzas_bin / (area_bin * Amax);
 
 //                //Find area of all pieces with ID in ID_disp
 //                double area_to_pack;
@@ -208,15 +200,14 @@ Construct_Solution (const char *bin_name, vector<PIEZA> pzas, double level,
 //                    of_select = (1.0/pow(area_pzas_bin/area_to_pack,0.5))* current_bin[bb].get_waste();
 //                }
 //                else of_select = 0.0;
-	      if (of_select > best_bin_use)
-		{
-		  best_bin_use = of_select;
-		  best_binIdx = bb;
-		}
+				if (of_select > best_bin_use) {
+					best_bin_use = of_select;
+					best_binIdx = bb;
+				}
 
-	      //============================================================
-	      //(F) Keep the min (Abs waste/ av Area of pieces in bin) × (area pieces to pack /#pieces left to pack)
-	      //============================================================
+				//============================================================
+				//(F) Keep the min (Abs waste/ av Area of pieces in bin) × (area pieces to pack /#pieces left to pack)
+				//============================================================
 //                double of_select = 0 ;
 //                of_select = current_bin[bb].get_waste();
 //                double av_area_bin=0;
@@ -241,9 +232,9 @@ Construct_Solution (const char *bin_name, vector<PIEZA> pzas, double level,
 //                    worst_bin_use = of_select;
 //                    best_binIdx = bb;
 //                }
-	      //============================================================
-	      //(G) Keep the min (Abs waste/ av Area of pieces in bin) × (area pieces to pack)
-	      //============================================================
+				//============================================================
+				//(G) Keep the min (Abs waste/ av Area of pieces in bin) × (area pieces to pack)
+				//============================================================
 //                double of_select = 0 ;
 //                of_select = current_bin[bb].get_waste();
 //                double av_area_bin=0;
@@ -264,22 +255,20 @@ Construct_Solution (const char *bin_name, vector<PIEZA> pzas, double level,
 //                    worst_bin_use = of_select;
 //                    best_binIdx = bb;
 //                }
-	    }                //endif
-	}                //endfor bb
-			 //Add current_bin to bin_sol and update placed pzas.
-      vector<PIEZA*> pbin = current_bin[best_binIdx].getPI ();
-      for (int p = 0; p < pbin.size (); p++)
-	{
-	  ID_placed.push_back (pbin[p]->getID ());
-	}
-      bin_sol.push_back (current_bin[best_binIdx]);
-      for (int i = 0; i < current_bin.size (); i++)
-	current_bin[i].empty_bin ();
-    }        //end while
+			}                //endif
+		}                //endfor bb
+		//Add current_bin to bin_sol and update placed pzas.
+		vector<PIEZA*> pbin = current_bin[best_binIdx].getPI();
+		for (int p = 0; p < pbin.size(); p++) {
+			ID_placed.push_back(pbin[p]->getID());
+		}
+		bin_sol.push_back(current_bin[best_binIdx]);
+		for (int i = 0; i < current_bin.size(); i++)
+			current_bin[i].empty_bin();
+	}        //end while
 
-  double ABins = 0;
-  for (int i = 0; i < bin_sol.size () - 1; i++)
-    {
+	double ABins = 0;
+	for (int i = 0; i < bin_sol.size() - 1; i++) {
 //        cout<<"\nBin: "<<bin_sol[i].getSize()<< " Pieces: ";
 //        for(int j = 0; j<bin_sol[i].getPI().size();j++)
 //        {
@@ -287,31 +276,28 @@ Construct_Solution (const char *bin_name, vector<PIEZA> pzas, double level,
 //            cout<< IDpza<<" " ;
 //        }
 //        cout<<"\n";
-      //===========================================
-      //Objective Function: Area Bin
-      //===========================================
-      ABins = ABins + bin_sol[i].getL () * bin_sol[i].getW ();
-    }
-  int nBins = bin_sol.size () - 1;
-  ABins = ABins
-      + bin_sol[nBins].getPropUtil () * bin_sol[nBins].getW ()
-	  * bin_sol[nBins].getL ();
-  of_value = A_bin + ABins;
+		//===========================================
+		//Objective Function: Area Bin
+		//===========================================
+		ABins = ABins + bin_sol[i].getL() * bin_sol[i].getW();
+	}
+	int nBins = bin_sol.size() - 1;
+	ABins = ABins
+			+ bin_sol[nBins].getPropUtil() * bin_sol[nBins].getW()
+					* bin_sol[nBins].getL();
+	of_value = A_bin + ABins;
 }
 
-double
-AreaToPack (NODE node, vector<PIEZA> pzas)
-{
-  double area = 0;
-  for (int i = 0; i < node.get_IDdisp ().size (); i++)
-    {
-      int id_to_pack = node.get_IDdisp ()[i];
-      int p = 0;
-      while (pzas[p].getID () != id_to_pack)
-	p++;
-      area = area + pzas[p].getArea ();
+double AreaToPack(NODE node, vector<PIEZA> pzas) {
+	double area = 0;
+	for (int i = 0; i < node.get_IDdisp().size(); i++) {
+		int id_to_pack = node.get_IDdisp()[i];
+		int p = 0;
+		while (pzas[p].getID() != id_to_pack)
+			p++;
+		area = area + pzas[p].getArea();
 
-    }
+	}
 
-  return area;
+	return area;
 }
